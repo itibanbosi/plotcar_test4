60871ebcd08ba8d3f98637ef6cae7ff70812629a
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2021-04-14 21:44:13 +0900

    更新 plotcar_test4.ts

--- plotcar_test4.ts
+++ plotcar_test4.ts
@@ -167,7 +167,7 @@ namespace eureka_plotter_car {
         microbit_wait=750;
         break;
         case microbit_version.Version2:
-        microbit_wait=2500;
+        microbit_wait=3000;
         break;
         case microbit_version.Test_A:
         microbit_wait=10000;
