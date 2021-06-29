ba779cc707c81547852362354ca9d14e229656c8
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2021-06-29 17:32:08 +0900

    更新 plotcar_test4.ts

--- plotcar_test4.ts
+++ plotcar_test4.ts
@@ -593,6 +593,7 @@ function  moter(kyori:number,R_zengo:number,L_zengo:number){
 
   //% color="#009A00"  weight=81 blockId=microbit2_decideLight block="m:bit光ｾﾝｻ値 |%limit| より暗い" group="3 microbitの光ｾﾝｻ"
   //% limit.min=0 limit.max=100
+  //% advanced=true
   export function microbit2_decideLight(limit: number) :boolean{
         if (input.lightLevel()/254*100  < limit) {            
           return true;
@@ -604,6 +605,7 @@ function  moter(kyori:number,R_zengo:number,L_zengo:number){
 
 
   //% color="#009A00"  weight=80 blockId=microbit2_denkitemp block="m:bit光ｾﾝｻ値" group="3 microbitの光ｾﾝｻ"
+  //% advanced=true
   export function microbit2_denkitemp():number{
 
         return Math.round(input.lightLevel()/254*100);
@@ -612,6 +614,7 @@ function  moter(kyori:number,R_zengo:number,L_zengo:number){
 
 
   //% color="#228b22"  weight=82 blockId=microbit2_denkiLED block="m:bit光ｾﾝｻの値を表示する" group="3 microbitの光ｾﾝｻ"
+  //% advanced=true
   export function microbit2_denkiLED(){
             basic.showNumber(Math.round(input.lightLevel()/254*100));
   }
@@ -621,6 +624,7 @@ function  moter(kyori:number,R_zengo:number,L_zengo:number){
 
 
   //% color="#009A00" weight=22 blockId=sonar_ping_2 block="きょりｾﾝｻ" group="3 超音波きょりｾﾝｻｰ"
+  //% advanced=true
   export function sonar_ping_2() :number{
     let  d1=0;
     let  d2=0;
@@ -654,6 +658,7 @@ function  moter(kyori:number,R_zengo:number,L_zengo:number){
 
   //% color="#009A00" weight=20 block="きょりが |%limit| cmより |%nagasa| " group="3 超音波きょりｾﾝｻｰ"
   //% limit.min=0 limit.max=30
+  //% advanced=true
   export function sonar_ping_3(limit: number ,nagasa:kyori): boolean {
     let  d1=0;
     let  d2=0;
@@ -691,16 +696,19 @@ function  moter(kyori:number,R_zengo:number,L_zengo:number){
 
 
   //% color="#f071bd" weight=30 blockId=auto_photo_R block="右ﾌｫﾄﾘﾌﾚｸﾀｰ" group="4 ﾌｫﾄﾘﾌﾚｸﾀｰ"
+  //% advanced=true
   export function phto_R() {
     return Math.round((pins.analogReadPin(AnalogPin.P10) / 1023) * 100);
   }
 
   //% color="#f071bd" weight=28 blockId=auto_photo_L block="左ﾌｫﾄﾘﾌﾚｸﾀｰ" group="4 ﾌｫﾄﾘﾌﾚｸﾀｰ"
+  //% advanced=true
   export function phto_L() {
     return Math.round((pins.analogReadPin(AnalogPin.P1) / 1023) * 100);
   }
 
   //% color="#d4b41f"  weight=26 block="右ﾌｫﾄﾘｸﾚｸﾀｰ値 |%limit_R| より小さい" group="4 ﾌｫﾄﾘﾌﾚｸﾀｰ"
+  //% advanced=true
   //% limit_R.min=0 limit_R.max=100
   export function photo_R(limit_R: number): boolean {
     if ((pins.analogReadPin(AnalogPin.P10) / 1023) * 100 < limit_R) {
@@ -712,6 +720,7 @@ function  moter(kyori:number,R_zengo:number,L_zengo:number){
 
   //% color="#d4b41f"  weight=27 block="左ﾌｫﾄﾘｸﾚｸﾀｰ値 |%limit_L| より小さい" group="4 ﾌｫﾄﾘﾌﾚｸﾀｰ"
   //% limit_L.min=0 limit_L.max=100
+  //% advanced=true
   export function photo_L(limit_L: number): boolean {
     if ((pins.analogReadPin(AnalogPin.P1) / 1023) * 100 < limit_L) {
       return true;
@@ -722,6 +731,7 @@ function  moter(kyori:number,R_zengo:number,L_zengo:number){
 
 //% color="#6041f1"  weight=23 block="右だけが |%wb| をふんだ時 しきい値 |%sikii| " group="4　センサー" group="4 ﾌｫﾄﾘﾌﾚｸﾀｰ"
 //% sence.min=10 sence.max=40
+//% advanced=true
   export function photo_R_out( wb: whiteblack,sikii:sence_select): boolean {
 　  if (sikii==sence_select.低感度)
     {
@@ -754,6 +764,7 @@ function  moter(kyori:number,R_zengo:number,L_zengo:number){
   }
 
   //% color="#6041f1"  weight=24 block="左だけが |%wb| をふんだ時 しきい値 |%sikii| " group="4 ﾌｫﾄﾘﾌﾚｸﾀｰ" 
+  //% advanced=true
   export function photo_L_out( wb: whiteblack ,sikii:sence_select): boolean {
 　  if (sikii==sence_select.低感度)
     {
@@ -788,6 +799,7 @@ function  moter(kyori:number,R_zengo:number,L_zengo:number){
     }
   }
   //% color="#6041f1"  weight=25 block="左右とも |%wb| をふんでいる時 しきい値 |%sikii| " group="4 ﾌｫﾄﾘﾌﾚｸﾀｰ"
+  //% advanced=true
   export function photo_LR_out(wb: whiteblack,sikii:sence_select): boolean {
 　  if (sikii==sence_select.低感度)
     {
